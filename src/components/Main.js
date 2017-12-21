6db26036413285a26d49fd831bb9155a088f1955
xuzhida <xuzhida@xuzhidadeMacBook-Pro.local>
2017-12-21 15:24:16 +0800

    webpack_model

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
